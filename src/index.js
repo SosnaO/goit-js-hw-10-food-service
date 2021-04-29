3926918f336cea01431cbc9744b6a7e93361d43d
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-04-29 21:20:46 +0300

    2

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import Menu from './menu.hbs'
 import lists from './menu.json'
 
 const PriceMenu = document.querySelector('.js-menu');
+
 const MenuCall = CreateMenuCall(lists);
 PriceMenu.insertAdjacentHTML('beforeend', MenuCall);
 function CreateMenuCall(lists) {
   //return lists.map(Menu).join('');
   return Menu(lists);
  }
-console.log(PriceMenu());
-//PriceMenu.insertAdjacentHTML('beforeend', MenuCall);
+//console.log(PriceMenu());
+PriceMenu.insertAdjacentHTML('beforeend', MenuCall);
